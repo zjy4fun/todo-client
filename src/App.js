9534b68eb268b7649fd68cbe662f09c30c3be1b1
zjy4fun <zjy4fun@163.com>
2022-07-14 11:42:59 +0800

    引入AntDesign组件库, 配置 craco

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Container from "./components/Container";
+import { Button } from 'antd';
+import './App.less'
 
 function App() {
     return (
         <div className="App">
             <Container/>
+            <Button type="Primary">Button</Button>
         </div>
     );
 }
